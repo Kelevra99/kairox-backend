59b439a9d6e7cbdfc86d0117d2f610a48c3b1a58
Юрий Никонов <yuriy@MacBook-Pro-Urij.local>
2026-04-20 22:12:13 +0300

    wire app config into site settings module

--- src/modules/site-settings/site-settings.module.ts
+++ src/modules/site-settings/site-settings.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
+import { AppConfigModule } from "@/modules/config/app-config.module";
 import { SiteSettingsController } from "./site-settings.controller";
 import { SiteSettingsService } from "./site-settings.service";
 
 @Module({
+  imports: [AppConfigModule],
   controllers: [SiteSettingsController],
   providers: [SiteSettingsService]
 })
